dfeb5fa03fbb91d902b58607848c6c03162d037f
urijan44 <teamsva360@gmail.com>
2022-07-09 19:37:55 +0900

    Add TMDBSevice result

--- ThisDataLayer/Sources/ThisDataLayer/Service/TMDBService.swift
+++ ThisDataLayer/Sources/ThisDataLayer/Service/TMDBService.swift
@@ -9,15 +9,37 @@ import Foundation
 import Combine
 
 struct TMDBService {
-  let provider = NetworkProvider<TMDB>()
+  enum Request {
+    enum searchMovie {
+      struct Message {
+        let text: String
+        let language: String
+        let region: String
+      }
+    }
+  }
 
-  func request() {
-    provider.request(.searchMovie(text: "", language: "", region: "")) { result in
+  enum Response {
+    enum searchMovie {
+      struct Message {
+        let data: Data?
+        let response: URLResponse?
+      }
+    }
+  }
+
+  private let provider = NetworkProvider<TMDB>()
+
+  func request(
+    message: Request.searchMovie.Message,
+    completion: @escaping (Result<Response.searchMovie.Message, Error>) -> Void) {
+    provider.request(
+      .searchMovie(text: message.text, language: message.language, region: message.region)) { result in
       switch result {
-        case .success(let data, let response):
-          ()
+        case .success((let data, let response)):
+          completion(.success(Response.searchMovie.Message(data: data, response: response)))
         case .failure(let error):
-          ()
+          completion(.failure(error))
       }
     }
   }
